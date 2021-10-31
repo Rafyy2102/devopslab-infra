ede6ce88220152ae9f5de3c71ca7af48b0c2b53b
Rafyy2102 <rafpromais@yahoo.com.br>
2021-10-31 01:51:49 -0300

    Terraform GCP Basics

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.90.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("/home/vagrant/gcpterraformkey.json")
+
+  project = "lab-devops-cloud-t3-rafyy"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
