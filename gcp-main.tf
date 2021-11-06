df92895095549ac3e838230cdd72ac39b5c2a36b
Rafyy2102 <rafpromais@yahoo.com.br>
2021-11-06 11:49:04 -0300

    App Engine Provision

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,23 +1,4 @@
-# Cria uma VM no Google Cloud
-resource "google_compute_instance" "rafyyvm" { # referencia o id do recurso
-
-  name         = "hellorafyy" #nome da maquina no google
-  machine_type = "n1-standard-1"
-  zone         = "us-central1-c"
-
-  # Defini a Imagem da VM
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-2004-focal-v20210927"
-    }
-  }
-
-  # Habilita rede para a VM com um IP público
-  network_interface {
-    network = "default" # Estamos usando a VPC default que já vem por padrão no projeto.
-
-    access_config {
-    // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
-    }
-  }
+resource "google_app_engine_application" "app" {
+  project     = "lab-devops-cloud-t3-rafyy"
+  location_id = "us-central"
 }
--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "3.90.0"
     }
   }
